c17e382c3531ecc05cd03588c330f757ac7b8e7c
Grigor Ohanyan <ohanyang183@gmail.com>
2023-10-16 21:34:46 +0400

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import { func } from './func';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello</h1> )
+root.render( <h1>{func()}</h1> )
