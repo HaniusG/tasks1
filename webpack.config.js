767ad38525849a723dc200fe197587707c49bde5
Grigor Ohanyan <ohanyang183@gmail.com>
2023-10-16 19:56:50 +0400

    commit

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
     mode: 'production',
     entry: path.resolve(__dirname, 'src', 'index.js'),
     output: {
-        filename: '[name].[contenthash]js',
+        filename: '[name].[contenthash].js',
         path: path.resolve(__dirname, 'build')
     }
 }
