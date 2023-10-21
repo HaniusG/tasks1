9f641ddfc6c2903623bc299289a58b1df0ce516d
Grigor Ohanyan <ohanyang183@gmail.com>
2023-10-21 19:52:03 +0400

    commit

--- src/components/InfoCard/InfoCard.tsx
+++ src/components/InfoCard/InfoCard.tsx
@@ -7,8 +7,8 @@ export default function InfoCard() {
     <div className='infoCard'>
       <div>
       <h1>Amazing Heading Text Right Here</h1>
-      <p>Expenses as material breeding building to in. Continentual so distrusts pronounce by unwilling listening. Things to taste on we manor. Him had use found hoped. Of distrust immediate enjoyment curiosity do. Marianne saw thoughts the humoured.</p>
-      <div>
+      <p className='cardParag'>Expenses as material breeding building to in. Continentual so distrusts pronounce by unwilling listening. Things to taste on we manor. Him had use found hoped. Of distrust immediate enjoyment curiosity do. Marianne saw thoughts the humoured.</p>
+      <div className='buttonDiv'>
         <Button/>
         <Button/>
       </div>
