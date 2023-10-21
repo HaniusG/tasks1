e01201dd1ee47ebb934c40493317da8a195a11e8
Grigor Ohanyan <ohanyang183@gmail.com>
2023-10-21 20:01:38 +0400

    commit

--- src/components/LanguageChange/LanguageChange.tsx
+++ src/components/LanguageChange/LanguageChange.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import './language-change.css'
 
 export default function LanguageChange() {
   return (
-    <select>
+    <select className='languageSelect'>
         <option value="EN">EN</option>
         <option value="AM">AM</option>
         <option value="RS">RS</option>
